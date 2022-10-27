ed75509dde059436ea10fc829d916e2abbd1cb5b
jacobsanosh <jacobsanosh@gmail.com>
2022-10-27 18:08:33 +0530

    navbar

--- frontend/src/pages/MainPage/MainPage.js
+++ frontend/src/pages/MainPage/MainPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './MainPage.css'
 import CarouselComponent from '../../components/Carousel/CarouselComponent'
+import Navbar from '../../components/Navbar/Navbar'
 const MainPage = () => {
   return (
+  
     <div>
+    <Navbar/>
     <CarouselComponent/>
     </div>
   )
