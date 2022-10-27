549898705860f1b4107354581a4ee90e3579dfb4
Adithya Ajith <4adhi007@gmail.com>
2022-10-28 04:33:50 +0530

    dashboard edit

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MainLayout from '../../components/MainLayout/MainLayout'
 import './Dashboard.css'
 import graph1 from '../../assets/jpeg/graph1.jpg'
-
+import propic from '../../assets/jpeg/propic.jpg'
 
 function Dashboard() {
   return (
@@ -19,12 +19,34 @@ function Dashboard() {
 
       </div>
       <div className='dashboard__premium'>
-          <img className='d__graph' src={graph1} />
+       <div className='dashboard__upgrade'>
+         <div className='dashboard__text'>
+            <h3>Upgrade your plan</h3>
+            <p>Try the unique features of our app</p>
+            
+         </div>
+         <div className='dashboard__btns'>
+            <div className='glow-on-hover'>Upgrade</div>
+         </div>
+          <div className='dashboard__anim'></div>
+
+       </div>
+          
       </div>
      </div>
      <div className='dashboard__profile'>
-    
-     <img className='d__graph' src={graph1} />
+      <div className='dashboard__namespic'>
+       <img className='propic' src={propic} />
+       <div className='dashboard__names'>
+         <h3>Catylyn</h3>
+         <h5>Caty</h5>
+       </div>
+      </div>
+     <div className='dashboard__details'>
+       <p>Age : 18</p>
+       <p>Gender : M</p>
+       <p>Job : Architect</p>
+     </div>
      </div>
     
     </div>
