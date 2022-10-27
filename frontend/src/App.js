9cb0493f2b273256b4871a7f7367c762dfe262c0
jacobsanosh <jacobsanosh@gmail.com>
2022-10-27 21:10:50 +0530

    adding carloan page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import './App.css';
 import { MainPage,Login } from './pages';
+import Carloan from './pages/CarLoan/Carloan';
 import Signup from './pages/Signup/Signup';
  
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path='/'element={<MainPage/>}/>
         <Route path='/login'element={<Login/>}/>
         <Route path='/signup'element={<Signup/>}/>
-        
+        <Route path='/carloan' element={<Carloan/>}/>
       </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/CarLoan/Carloan.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Carloan.css'
+function Carloan() {
+  return (
+    <div class="Carloan__container">
+        <div class="Carloan__left">
+            <h1>Car loan calulation</h1>
+        </div>
+        <div class="Carloan__right">
+            <h1>right section</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Carloan
