3ec2f228ace962da68d0f7c06c0e690cf2234c4c
Abhishek Kurian <abhishekkurian12@gmail.com>
2022-10-27 19:27:43 +0530

    test

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 from dataclasses import field, fields
 from importlib.metadata import files
 from rest_framework import serializers
-from .models import (User,UserDetail,UserOpinionAgent,TrendingInsuranceAgents,CarLoan,HousingLoan)
+from .models import (User,UserDetail,UserOpinionAgent,TrendingInsuranceAgents,CarLoan,HousingLoan,Medical_expense,Electronic_Devices)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken,TokenError
 
@@ -53,12 +53,12 @@ class HousingLoanSerializer(serializers.ModelSerializer):
         model=HousingLoan
         fields='__all__'
 
-class Medical_expense(serializers.ModelSerializer):
+class Medical_expenseSerializer(serializers.ModelSerializer):
     class Meta:
         model=Medical_expense
         fields='__all'
 
-class Electronic_Devices(serializers.ModelSerializer):
+class Electronic_DevicesSerializer(serializers.ModelSerializer):
     class Meta:
         model=Electronic_Devices
         fields='__all'
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from .views import (BlacklistTokenView,LoggedInUserView,RegisterView,UserDetailViewSet,
-                    HousingLoanViewset,CarLoanViewset,UserOpinionAgentViewset,TrendingInsuranceAgentViewSet,MedicalExpenseViewset,ElectronicDevicesViewset)
+                    HousingLoanViewset,CarLoanViewset,UserOpinionAgentViewset,TrendingInsuranceAgentViewSet,ElectronicDevicesViewset,MedicalViewset)
 router=DefaultRouter()
 router.register('register',RegisterView,basename='register')
 router.register('user-detail',UserDetailViewSet,basename='user-detail')
@@ -12,8 +12,8 @@ router.register('opinions',UserOpinionAgentViewset,basename='opinions')
 router.register('trending',TrendingInsuranceAgentViewSet,basename='trending')
 router.register('car-loans',CarLoanViewset,basename='car-loans')
 router.register('housing-loans',HousingLoanViewset,basename='housing-loans')
-router.register('Medical-Expense',MedicalExpenseViewset,basename='Medical-Expense')
-router.register('Electronic-Devices',ElectronicDevicesViewset,basename='Electronic-Devices')
+router.register('electronic-devices',ElectronicDevicesViewset,basename='electronic-devices')
+router.register('medical-ins',MedicalViewset,basename='medical-ins')
 
 urlpatterns = [
     path('',include(router.urls)),
