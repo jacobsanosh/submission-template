78c2002810f2f5686ad47f4fca7fa739aa8c734a
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-10-27 15:16:00 +0530

    admin edits

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import(User,UserDetail)
+from .models import(User,UserDetail,TrendingInsuranceAgents,UserOpinionAgent)
 # Register your models here.
 admin.site.register(User)
 admin.site.register(UserDetail)
+admin.site.register(TrendingInsuranceAgents)
+admin.site.register(UserOpinionAgent)
