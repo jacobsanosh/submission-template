f46274b87851fb2d80c2fada6529e814fe4d5e9a
Adithya Ajith <4adhi007@gmail.com>
2022-10-27 17:25:38 +0530

    auth signup

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -26,9 +26,9 @@ const[password,setPassword]=useState("");
 		<div class="Login__form">
 			<img class='Login__img' src="https://i.pinimg.com/236x/4d/a8/bb/4da8bb993057c69a85b9b6f2775c9df2.jpg" alt="profile"/>
 			<p class='Login__para'>Already have an Account? <a class="Login__a" href="/"> Sign in</a></p>
-			<input type="email" autocomplete placeholder="Email"/>
-            <input type="text" autocomplete placeholder="username"/>
-			<input type="password" placeholder="Password"/>
+			<input type="email" autocomplete placeholder="Email"  onChange={(e)=>setEmail(e.target.value)} />
+            <input type="text" autocomplete placeholder="username" onChange={(e)=>setUsername(e.target.value)}  />
+			<input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
 			<button class='Login__btn' type="submit">Sign Up</button>
 		</div>
 	</div>
