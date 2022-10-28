58d703071a3630feb22ee8149dbbd30f54066858
Adithya Ajith <4adhi007@gmail.com>
2022-10-28 07:29:09 +0530

    dropdown

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Navbar.css'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 function Navbar() {
   return (
     <nav class="Navbar__navbar">
@@ -8,7 +10,11 @@ function Navbar() {
         <ul class="Navbar__ul">
           <li><a href="/">Home</a></li>
           <li><a href="/">About</a></li>
-          <li><a href="/">Specs</a></li>
+          <DropdownButton id="dropdown-basic-button" title="Calculate">
+          <Dropdown.Item className='drop__items' href="/carLoan">Car Loan</Dropdown.Item>
+          <Dropdown.Item className='drop__items' href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item className='drop__items' href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
 		    <li><a href="/login">Login</a></li> 
         </ul>
       </div>
